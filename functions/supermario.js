42d8215f4f1ef48863177f09750d3804ed3c0f15
rakanishu <rakanishudev@gmail.com>
2022-03-04 16:02:30 +0200

    cloud function

---
+++ functions/supermario.js
@@ -0,0 +1,9 @@
+exports.handler = async () => {
+    console.log('function ran')
+    const data = {name: 'mario', age :35, job: 'plumber'}
+    //retunr response to browser
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }
+}
